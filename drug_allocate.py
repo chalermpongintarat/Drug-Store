76a8d2c6d04f1c1609eeee06ccf7ed2bb4cda7d7
Chalermpong Intarat <32490473+chalermpongintarat@users.noreply.github.com>
2023-05-09 14:39:26 +0700

    Add files via upload

---
+++ drug_allocate.py
@@ -0,0 +1,34 @@
+with open("/Users/chalermpong/Desktop/drug_store/drug_list.txt", "r") as file: 
+    lines = file.read().split("\n")
+    
+    find_1 = "(" # () {} [] ' , - _
+    find_2 = "{"
+    find_3 = "["
+    find_4 = "'"
+    find_5 = ","
+    find_6 = "-"
+    find_7 = "_"
+
+    with_symbol = []
+    without_symbol = []
+
+    with_symbol_line = ""
+    without_symbol_line = ""
+
+    for line in lines:
+        if find_1 in line or find_2 in line or find_3 in line or find_4 in line or find_5 in line or find_6 in line or find_7 in line:
+            with_symbol.append(line)
+        else:
+            without_symbol.append(line)
+
+    f = open("/Users/chalermpong/Desktop/drug_store/drug_with_symbol.txt", "w")
+    for line in with_symbol:
+        f.write(str(line))
+        f.write("\n")
+    f.close()
+
+    f = open("/Users/chalermpong/Desktop/drug_store/drug_without_symbol.txt", "w")
+    for line in without_symbol:
+        f.write(str(line))
+        f.write("\n")
+    f.close()
